48bfeed725f2cfb17672faf04a329a7cc987cd77
Evilroxy <cvanelstraete@edenschool.fr>
2024-01-18 17:02:51 +0100

    made fetch

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
-const app = document.querySelector('#app');
-let body = document.body
-let article = document.createElement('Article')
-let currency = "€"
-let price = document.createElement('p')
-price.textContent=  '100' + currency
+let response = await fetch("https://dummyjson.com/products");
+let responseAsJson = await response.json();
+const products = responseAsJson.products;
 
-let heading2 = document.createElement('h2')
-heading2.textContent = "Ceinture"
+console.log(products);
 
-let placeholder = document.createElement("img")
-placeholder.setAttribute("src", "/public/image.jpg")
+function articles(products) {
+  for (let prod = 0; prod <= products.length, prod++; ) {
+    price.textContent = products.price;
+    placeholder.setAttribute("src", products.images);
+    let currency = "€";
+    heading2.textContent = products.title;
+  }
+}
 
-body.appendChild(article)
-article.appendChild(heading2)
-article.appendChild(placeholder)
-article.appendChild(price)
-
-
-
-
-
+const app = document.querySelector("#app");
+let body = document.body;
+let article = document.createElement("Article");
+let price = document.createElement("p");
+let heading2 = document.createElement("h2");
+let placeholder = document.createElement("img");
+body.appendChild(article);
+article.appendChild(heading2);
+article.appendChild(placeholder);
+article.appendChild(price);
