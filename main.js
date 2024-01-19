68d0877518f840c3d75d3374d5f16cc9707b94ec
Evilroxy <cvanelstraete@edenschool.fr>
2024-01-19 09:53:01 +0100

    added more informations

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 let response = await fetch("https://dummyjson.com/products");
 let responseAsJson = await response.json();
 const products = responseAsJson.products;
+let placeholder;
 
 function articles(p) {
   for (let prod = 0; prod < products.length; prod++) {
     let price = document.createElement("p");
     price.textContent = products[prod].price;
-    let placeholder = document.createElement("img");
+    placeholder = document.createElement("img");
     placeholder.setAttribute("src", products[prod].images[0]);
     let currency = "€";
     let heading2 = document.createElement("h2");
@@ -16,31 +17,20 @@ function articles(p) {
     article.appendChild(placeholder);
     article.appendChild(price);
     document.querySelector("#app").appendChild(article);
+   
   }
 }
-
 articles(products);
 
-// let response = await fetch("https://dummyjson.com/products");
-// let responseAsJson = await response.json();
-// const products = responseAsJson.products;
 
-// function articles(p) {
-//   for (let prod = 0; prod <= products.length, prod++; ) {
-//     price.textContent = products.price;
-//     placeholder.setAttribute("src", products.images[0]);
-//     let currency = "€";
-//     heading2.textContent = products.title;
-//   }
-// }
+function plusDimg() {
+  placeholder.addEventListener('click', console.log("test"));
+  let length = products.images
 
-// const app = document.querySelector("#app");
-// let body = document.body;
-// let article = document.createElement("Article");
-// let price = document.createElement("p");
-// let heading2 = document.createElement("h2");
-// let placeholder = document.createElement("img");
-// body.appendChild(article);
-// article.appendChild(heading2);
-// article.appendChild(placeholder);
-// article.appendChild(price);
+  for (let img = 0; img < length; img++) {
+  let popUp = document.createElement('div');
+  popUp.textContent = products[img].images;
+  div.appendChild(placeholder)
+
+  }
+}
